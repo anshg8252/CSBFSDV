0d583c9b4f77023bd32ee2fb404efe599fc28170
anshg8252 <anshg6910@gmail.com>
2025-11-12 10:22:54 +0530

    React router dom and event handling

--- ReactWork/reactviteapp/src/App.jsx
+++ ReactWork/reactviteapp/src/App.jsx
@@ -8,9 +8,13 @@ import StateHandling from './component/StateHandling.jsx'
 import ImageManipulation from './component/imagemanipulation.jsx'
 import Login from './pages/Login.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Registration from './pages/Registration.jsx'
+import Dashboard from './pages/Dashboard.jsx'
+import MainLayout from './pages/MainLayout.jsx'
 
 function App() {
   // const [count, setCount] = useState(0)
+  const [data, setData] = useState();
 
   return (
     // <div className="container">
@@ -22,9 +26,15 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path='/login' element={<Login/>}></Route>
+          <Route path='/login' element={<Login logData={data}/>}></Route>
+          <Route path='/register' element={<Registration regData={setData}/>}></Route>
+          <Route path='/dashboard' element={<Dashboard/>}></Route>
+          <Route path='/' element={<MainLayout/>}></Route>
         </Routes> 
       </BrowserRouter>
+      {/* <h2>
+        {JSON.stringify(data)}
+      </h2> */}
     </div>
   )
 }
--- ReactWork/reactviteapp/src/pages/Login.jsx
+++ ReactWork/reactviteapp/src/pages/Login.jsx
@@ -1,25 +1,52 @@
 import React from 'react'
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function Login() {
+function Login({logData}) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const navigate = useNavigate();
+
+  function verification(e){
+    
+    e.preventDefault();
+    // console.log(logData.email);
+    // alert("Hi " + email + " " + password);
+
+    if(logData.email === email){
+      if(logData.password === password){
+        alert("Login Successful and navigating to Dashboard");
+        navigate('/dashboard');
+      }
+      else{
+        alert("Invalid Password");
+      }
+    }
+    else{
+      alert("Invalid Email");
+    }
+  }
+
   return (
     <div>
         <h2>Login Form</h2>
-        <form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        <form onSubmit={verification}>
+  <div className="form-group">
+    <label htmlFor="exampleInputEmail1">Email address</label>
+    <input onChange={(e)=>setEmail(e.target.value)} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
   </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1"/>
+  <div className="form-group">
+    <label htmlFor="exampleInputPassword1">Password</label>
+    <input onChange={(e)=>setPassword(e.target.value)} type="password" className="form-control" id="exampleInputPassword1"/>
   </div>
-  <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  <div className="form-group form-check">
+    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button onClick={verification} type="submit" className="btn btn-primary">Submit</button>
 </form>
     </div>
   )
