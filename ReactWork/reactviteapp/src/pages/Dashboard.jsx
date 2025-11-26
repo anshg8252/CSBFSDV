26be95e2fe1f07eb7af8915b4ea3b7e5ab25eba7
anshg8252 <anshg6910@gmail.com>
2025-11-26 10:28:52 +0530

    React component lifecycle

--- ReactWork/reactviteapp/src/pages/Dashboard.jsx
+++ ReactWork/reactviteapp/src/pages/Dashboard.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
 
 function Dashboard() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const serverresponse = await fetch('https://fakestoreapi.com/products');
+      const jsonresponse = await serverresponse.json();
+      console.log(jsonresponse);
+      setData(jsonresponse);
+    }
+    fetchData();
+  }, []);
+
   return (
-    <div>Dashboard</div>
-  )
+    <div>
+      {data.length === 0 ? (
+        <h1>Data could not fetch</h1>
+      ) : (
+        <div>
+          {data.map((ele) => (
+            <div key={ele.id} style={{height: 300, width: 300 }}>
+              <img src={ele.image} alt="" height={100} width={100} />
+              <h2>{ele.title}</h2>
+              <button>Add to Cart</button>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
